0c176d830b50ce69f2d17f23fc923916a3220b6f
fhun <thummaporn.nimpanomprasert@smava.de>
2026-03-13 01:32:06 +0100

    feature: adjust intermediate layer - int_activity

--- models/intermediate/int_activity/int_activity.sql
+++ models/intermediate/int_activity/int_activity.sql
@@ -1,29 +1,17 @@
--- This intermediate model joins the activity and activity types staging models
--- to provide a more comprehensive view of activities, including their activity_type_name (Sales Call 1, Sales Call 2) and related information.
+-- This intermediate model is a pass-through for activities.
+-- Currently no transformations are needed beyond what staging provides.
+-- This layer exists as a placeholder for future transformations and to maintain a consistent structure in the data pipeline.
 
 with activity as (
     select
         *
     from {{ ref('stg_activity') }}
-),
-activity_types as (
-    select
-        *
-    from {{ ref('stg_activity_types') }}
-),
-w_joined as (
-    select
-        a.activity_id,
-        a.activity_type,
-        at.activity_type_name,
-        a.assigned_to_user_id,
-        a.deal_id,
-        a.is_done,
-        a.due_at
-    from activity as a
-    left join activity_types as at -- To prevent losing any activity records that do not have a matching activity type in the future.
-        on a.activity_type = at.activity_type
 )
 select
-    *
-from w_joined
+    a.activity_id,
+    a.activity_type,
+    a.assigned_to_user_id,
+    a.deal_id,
+    a.is_done,
+    a.due_at
+from activity as a
